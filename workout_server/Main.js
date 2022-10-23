2f9c741000ea462d90b8a92e6293eca7aa013799
Raven Clodd <raven.c1@outlook.com>
2022-10-23 17:08:34 +1000

    Basic Login but react Rendering error

--- workout_server/Main.js
+++ workout_server/Main.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const port = 5000;
 const path = require('path');
-
+app.use(cors());
 app.use(express.static(path.join(__dirname, "build")));
 
-let data = { "squat": [10, 15, 20, 25], "bench": [30, 35, 40, 45], "deadlift": [50, 55, 60, 65] };
+const data = { "squat": [10, 15, 20, 25], "bench": [30, 35, 40, 45], "deadlift": [50, 55, 60, 65] };
+const users = ["raven", "jim", "bob", "jane"];
 app.get("/", (req, res) => {
+    console.log("recieved get request");
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get("/registerUser", (req, res) => {
-    console.log("registering user");
+app.post("/registerUser", (req, res) => {
+    console.log("registering user - ");
     res.send(data);
 })
 
