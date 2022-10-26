88ecb6027cb3b31f545546407cfbd9287e4b85e4
Raven Clodd <raven.c1@outlook.com>
2022-10-26 20:03:23 +1000

    Refactored retrieving data into ProgressChart Component

--- workout_server/Main.js
+++ workout_server/Main.js
@@ -6,7 +6,8 @@ const path = require('path');
 app.use(cors());
 app.use(express.static(path.join(__dirname, "build")));
 
-const data = { "squat": [10, 15, 20, 25], "bench": [30, 35, 40, 45], "deadlift": [50, 55, 60, 65] };
+//const data = { "squat": [10, 15, 20, 25], "bench": [30, 35, 40, 45], "deadlift": [50, 55, 60, 65] };
+const projectedWeight = [100, 102.5, 107.5, 112.5];
 const users = ["raven", "jim", "bob", "jane"];
 app.get("/", (req, res) => {
     console.log("recieved get request");
@@ -17,7 +18,7 @@ app.get("/", (req, res) => {
 // app to register user, and send the client the users data
 app.post("/registerUser", (req, res) => {
     console.log("registering user - ");
-    res.send(data);
+    res.send(projectedWeight);
 })
 
 app.listen(port);
